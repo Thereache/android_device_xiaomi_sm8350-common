dd6da871f98b2a90e4c2795f11b90fc909760b1d
Anushek Prasal <anushekprasal@gmail.com>
2025-01-12 14:09:16 +0100

    sm8350-common: import OnePlus 9R dolby
    Co-Authored-By: HELLBOY017 <abhaygill017@gmail.com>
    Co-Authored-By: Omkar Chandorkar <gotenksIN@aosip.dev>
    Co-Authored-By: ziasam <ziauddinsameer@gmail.com>
    Co-Authored-By: chandu078 <chandudyavanapelli03@gmail.com>
    Co-Authored-By: Kanishk <kanishkthederp@gmail.com>
    Co-Authored-By: Frost <159105703+Karan-Frost@users.noreply.github.com>

--- common.mk
+++ common.mk
@@ -161,6 +161,27 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     dalvik.vm.image-dex2oat-cpu-set=0,1,2,3,4,5,6,7 \
     dalvik.vm.image-dex2oat-threads=8
 
+# Dolby
+PRODUCT_PACKAGES += \
+    libcodec2_soft_common.vendor \
+    libstagefright_omx \
+    libstagefright_foundation \
+    libstagefright_softomx.vendor
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/dolby/dax-default.xml:$(TARGET_COPY_OUT_ODM)/etc/dolby/dax-default.xml \
+    $(LOCAL_PATH)/configs/dolby/media_codecs_dolby_audio.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_dolby_audio.xml \
+    $(LOCAL_PATH)/configs/dolby/config-com.dolby.daxappui.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/config-com.dolby.daxappui.xml \
+    $(LOCAL_PATH)/configs/dolby/config-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/config-com.dolby.daxservice.xml \
+    $(LOCAL_PATH)/configs/dolby/hiddenapi-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/sysconfig/hiddenapi-com.dolby.daxservice.xml \
+    $(LOCAL_PATH)/configs/dolby/privapp-com.dolby.daxappui.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-com.dolby.daxappui.xml \
+    $(LOCAL_PATH)/configs/dolby/privapp-com.dolby.daxservice.xml:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/permissions/privapp-com.dolby.daxservice.xml
+
+# Dolby VNDK libs
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v33/arm/arch-arm-armv7-a-neon/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib/libstagefright_foundation-v33.so \
+    prebuilts/vndk/v33/arm64/arch-arm64-armv8-a/shared/vndk-core/libstagefright_foundation.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libstagefright_foundation-v33.so
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
