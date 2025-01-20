56659cf608faddf3e2982e6de3a13559fd89d93b
LuK1337 <priv.luk@gmail.com>
2025-01-20 08:14:32 +0200

    sm8350-common: Build gralloc.qcom

--- common.mk
+++ common.mk
@@ -141,6 +141,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.graphics.mapper@3.0-impl-qti-display \
     android.hardware.graphics.mapper@4.0-impl-qti-display \
+    gralloc.qcom \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.composer-service \
     vendor.qti.hardware.display.composer-service.rc \
